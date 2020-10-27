4be61bb4991a6a14177b398f2b9a3be9f25a7f6b
garynye <72671979+garynye@users.noreply.github.com>
2020-10-27 15:25:00 -0500

    comment

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import navbar from '../components/navbar';
+
+const About = () => (
+
+    <div>
+            <navbar/>
+            <h1>About to BitzPrice</h1>
+            <p>Application to view Bitcoin Prices</p>
+    </div>
+    );
+
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import navbar from '../components/navbar';
+
+const Index = () => (
+
+    <div>
+        <navbar/>
+        <h1>Welcome to BitzPrice</h1>
+    </div> 
+    );
+
+
+export default Index;
